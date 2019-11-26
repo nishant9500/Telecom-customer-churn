b5e8bc04eb227a4775b83ef229d89205f6bb3c68
Nishant Kumar <nishant.kumar9500@gmail.com>
2019-11-25 22:42:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import numpy as np
+import pandas as pd
 from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
+columns=['const', 'PLAN_COST', 'TOTAL_PLAN_QTY', 'TOTAL_PLAN_AMOUNT',
+       'VOICE_CNT', 'VOICE_MINUTES_LOCAL', 'SMS_CNT', 'TOTALOCTETSUNIT1',
+       'UNITS_MONEY']
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -14,9 +18,12 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    #int_features = [int(x) for x in request.form.values()]
+    #final_features = [np.array(int_features)]
+    payload = request.get_json()
+    obs = pd.DataFrame([payload], columns=columns).astype(dtypes)
+    #proba = pipeline.predict_proba(obs)[0, 1]
+    prediction = model.predict(obs)
 
     output = round(prediction[0], 2)
 
